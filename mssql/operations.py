1f5be8690630cd9a5c3363341209fd2d78a02086
pbcornelius <cornelius@rsm.nl>
2026-04-01 11:12:48 +0200

    native uuid

--- mssql/operations.py
+++ mssql/operations.py
@@ -150,6 +150,9 @@ class DatabaseOperations(BaseDatabaseOperations):
             value = float(value)
         return value
 
+    def convert_uuidfield_value(self, value, expression, connection):
+        return value
+
     def convert_booleanfield_value(self, value, expression, connection):
         return bool(value) if value in (0, 1) else value
 
@@ -344,6 +347,8 @@ class DatabaseOperations(BaseDatabaseOperations):
             converters.append(self.convert_datetimefield_value)
         elif internal_type == 'FloatField':
             converters.append(self.convert_floatfield_value)
+        elif internal_type == 'UUIDField':
+            converters.append(self.convert_uuidfield_value)
         elif internal_type in ('BooleanField', 'NullBooleanField'):
             converters.append(self.convert_booleanfield_value)
         return converters
